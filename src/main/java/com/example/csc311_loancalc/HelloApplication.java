06b7c2e5224bd954c3430035066b818e1d1bcbe8
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2023-10-02 00:54:46 -0400

    Updated Comments and Javadocs

--- src/main/java/com/example/csc311_loancalc/HelloApplication.java
+++ src/main/java/com/example/csc311_loancalc/HelloApplication.java
@@ -4,22 +4,43 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-
-
 import java.io.IOException;
 
+/**
+ * The main class for the Loan Calculator application.
+ */
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Loan Calculator");
-        stage.setScene(scene);
-        stage.show();
-    }
 
+    /**
+     * The entry point of the application.
+     *
+     * @param args Command-line arguments (not used in this application).
+     */
     public static void main(String[] args) {
         launch();
+    }
+
+    /**
+     * Start method called when launching the JavaFX application.
+     *
+     * @param stage The primary stage for the application.
+     * @throws IOException If there is an error loading the FXML file.
+     */
+    @Override
+    public void start(Stage stage) throws IOException {
+        // Load the FXML layout file for the user interface
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
+        // Create a scene with the loaded FXML content
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        // Set the title of the application window
+        stage.setTitle("Loan Calculator");
+
+        // Set the scene for the stage
+        stage.setScene(scene);
+
+        // Show the stage (i.e., the application window)
+        stage.show();
     }
 }
--- src/main/java/com/example/csc311_loancalc/HelloController.java
+++ src/main/java/com/example/csc311_loancalc/HelloController.java
@@ -5,10 +5,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+/**
+ * Controller class for the Loan Calculator application.
+ */
 public class HelloController {
 
     @FXML
     private Label loanAmount;
+
     @FXML
     private TextField loanAmountInput;
 
@@ -39,34 +43,52 @@ public class HelloController {
     @FXML
     private Button calculateButton;
 
-
+    /**
+     * Handles the event when the "Calculate" button is clicked.
+     */
     @FXML
     protected void onCalculateButtonClick() {
+        // Parse input values from text fields
         double loanAmount = Double.parseDouble(loanAmountInput.getText());
         double loanPeriod = Double.parseDouble(loanPeriodInput.getText());
         double interestRate = Double.parseDouble(interestRateInput.getText());
 
+        // Calculate monthly and total payments
         double monthlyPayment = calculateMonthlyPayment(loanAmount, loanPeriod, interestRate);
         double totalPayment = calculateTotalPayment(monthlyPayment, loanPeriod);
 
+        // Display the results
         monthlyPaymentOutput.setText(String.format("$%.2f", monthlyPayment));
         totalPaymentOutput.setText(String.format("$%.2f", totalPayment));
     }
 
+    /**
+     * Calculates the total payment over the loan period.
+     *
+     * @param monthlyPayment The monthly payment amount.
+     * @param loanPeriod     The loan period in years.
+     * @return The total payment over the loan period.
+     */
     private double calculateTotalPayment(double monthlyPayment, double loanPeriod) {
         return monthlyPayment * 12 * loanPeriod;
     }
 
+    /**
+     * Calculates the monthly payment for the loan using the formula.
+     *
+     * @param loanAmount    The initial loan amount.
+     * @param loanPeriod    The loan period in years.
+     * @param interestRate  The annual interest rate.
+     * @return The calculated monthly payment.
+     */
     private double calculateMonthlyPayment(double loanAmount, double loanPeriod, double interestRate) {
         // Convert annual interest rate to monthly rate and number of months
         double monthlyInterestRate = (interestRate / 100) / 12;
         double numberOfMonths = loanPeriod * 12;
 
-        // Calculate monthly payment
+        // Calculate monthly payment using the formula
         double monthlyPayment = (loanAmount * monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -numberOfMonths));
 
         return monthlyPayment;
     }
-
-
 }
